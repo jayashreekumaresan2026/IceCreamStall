5940578455aee4d0260ec056f81bbf646c7038a6
Jayashree <jayashree.kammu@gmail.com>
2019-06-24 20:55:33 +0530

    get icecream options from the user and invoke the cost method

--- src/com/company/Application.java
+++ src/com/company/Application.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
@@ -20,5 +21,18 @@ public class Application {
         for (int i = 0; i < iceCreamList.size(); i++) {
             iceCreamList.get(i).iceCreamWithCost();
         }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select your IceIcream....");
+        System.out.print("Choose your IceCream Type :");
+        String userType = scanner.nextLine();
+        System.out.print("Choose your flavor :");
+        String userFlavor = scanner.nextLine();
+        System.out.print("Quantity :");
+        int quantity = Integer.parseInt(scanner.nextLine());
+        int iceCreamCost=0;
+        for (int i = 0; i < iceCreamList.size(); i++) {
+            iceCreamCost=iceCreamStall.calculateIceCreamCost(userType, userFlavor,quantity,iceCreamList);
+        }
+        System.out.println("Cost of your selected IceCream :" + (iceCreamCost*quantity));
     }
 }
